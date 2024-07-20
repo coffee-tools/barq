df0f6f8203b886e672fe1a462157691afed7aa25
raehat <raehatsinghnanda@gmail.com>
2024-07-20 17:14:28 +0700

    fix bugs

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,16 +1,55 @@
 import os
-
+import logging
 from pyln.testing.fixtures import *  # noqa: F403
+from pyln.testing.utils import only_one
+from pyln.client import Millisatoshi
+
+# Configure logging
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 barq_binary = os.path.join(os.path.dirname(__file__), "../target/debug/barq-plugin")
 
-
 def test_init(node_factory):
-    """This is just a monkey test to check that the
-    test env is configured fine"""
+    """This is just a monkey test to check that the test env is configured fine"""
     node = node_factory.get_node(
         options={
             "plugin": barq_binary,
         }
     )
     assert True
+
+def test_pay_amounts(node_factory):
+    """We steal this from core lightning test_pay.py, and we are try just to pay an invoice with amount"""
+    # l1, l2 = node_factory.line_graph(2, opts=[{"plugin": barq_binary }, { "plugin": barq_binary}])
+    l1, l2 = node_factory.line_graph(2)
+    inv = l2.rpc.invoice(Millisatoshi("123sat"), 'test_pay_amounts', 'description')['bolt11']
+
+    invoice = only_one(l2.rpc.listinvoices('test_pay_amounts')['invoices'])
+    logger.debug(f"Initial invoice: {invoice}")
+
+    assert invoice['amount_msat'] == Millisatoshi(123000)
+
+    l1.rpc.call("barqpay", {"bolt11_invoice": inv})
+
+    invoice = only_one(l2.rpc.listinvoices('test_pay_amounts')['invoices'])
+    logger.debug(f"Invoice after payment: {invoice}")
+
+    assert invoice['amount_received_msat'] >= Millisatoshi(123000)
+
+def test_pay_without_amounts(node_factory):
+    """We are going to test that we can pay an invoice without amount"""
+    l1, l2 = node_factory.line_graph(2)
+    inv = l2.rpc.invoice("any", 'test_pay_without_amounts', 'description')['bolt11']
+
+    invoice = only_one(l2.rpc.listinvoices('test_pay_without_amounts')['invoices'])
+    logger.debug(f"Initial invoice: {invoice}")
+
+    assert invoice['amount_msat'] == Millisatoshi(123000)
+
+    l1.rpc.call("barqpay", {"bolt11_invoice": inv})
+
+    invoice = only_one(l2.rpc.listinvoices('test_pay_without_amounts')['invoices'])
+    logger.debug(f"Invoice after payment: {invoice}")
+
+    assert invoice['amount_received_msat'] >= Millisatoshi(123000)
